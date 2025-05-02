76d6f696babc451a66f3ce2a8866c561e9f35fd4
sm-muhaimen <amuhaimenpro829@gmail.com>
2025-05-02 23:48:31 +0600

    package and config file issue fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // This tells Next.js to generate static HTML
 };
 
 export default nextConfig;
